c5742c8caba08026e03d52dd28891acadd61263e
KesalePratiksha <kesaleprati2@gmail.com>
2022-09-20 21:22:42 +0530

    added content in ClassA

--- src/main/java/orangeHRM/ClassAPrati.java
+++ src/main/java/orangeHRM/ClassAPrati.java
@@ -1,8 +1,13 @@
 package orangeHRM;
 
 public class ClassAPrati {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("hii am very smart");
+		
+		System.out.println("hii prati are you ok");
+		
+		System.out.println(" Hello");
 	}
 
 }
